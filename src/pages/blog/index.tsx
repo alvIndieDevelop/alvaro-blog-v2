2f404bcfac86618aa1c204ddb3b7a9d0789a623d
Alvaro Martin Caballero <a.martin@lhc.host>
2023-03-23 17:48:43 -0500

    agregando notion api y seccion de blog

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,30 @@
-export default function blog() {
-  return <div>Blog page</div>;
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { BlogPost } from "@/@types/schema";
+import NotionService from "@/services/notion-services";
+import BlogCard from "@/components/BlogCard";
+
+export default function blog({
+  posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  if (posts.length > 0) {
+    return (
+      <div>
+        Blog page
+        {posts.map((post: BlogPost) => (
+          <BlogCard key={post.id} post={post} />
+        ))}
+      </div>
+    );
+  }
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const notionServices = new NotionService();
+  const posts = await notionServices.getPublishedBlogPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
