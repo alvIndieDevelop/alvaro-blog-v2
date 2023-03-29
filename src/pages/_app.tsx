2edd4941786fb97523e3c32fa947015d57497079
Alvaro Martin Caballero <alvindiedevelop@gmail.com>
2023-03-28 23:02:37 -0500

    mejorando resumen y blog, mejoras responsives

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import MainLayout from "@/components/layouts/MainLayout";
+import { analytics } from "@/utils/firebase";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    analytics;
+  }, []);
+
   return (
     <MainLayout>
       <Component {...pageProps} />
