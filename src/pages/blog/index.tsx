93a936c8dc3564f96fe68962d89684716b500e01
Alvaro Martin Caballero <alvindiedevelop@gmail.com>
2024-10-07 00:48:29 -0500

    agregando filtros en blog

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { BlogPost } from "../../@types/schema";
 import NotionService from "../..//services/notion-services";
@@ -8,6 +8,26 @@ import Head from "next/head";
 export default function blog({
   posts,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  // state
+  const [selectedCategory, setSelectedCategory] = useState<string>("All");
+
+  // obtener las categorias unicas de los posts.
+  const uniqueCategories = [
+    "All",
+    ...Array.from(
+      new Set(
+        posts.flatMap((post: BlogPost) => post.tags.map((tag) => tag.name))
+      )
+    ),
+  ];
+
+  const filteredPosts =
+    selectedCategory === "All"
+      ? posts
+      : posts.filter((post: BlogPost) =>
+          post.tags.some((tag) => tag.name === selectedCategory)
+        );
+
   return (
     <>
       <Head>
@@ -35,11 +55,31 @@ export default function blog({
               Aqui comparto mis ideas, articulos y mas!
             </p>
           </div>
+
+          {/* Filtro de Categorías */}
+          <div className="mt-6 flex space-x-4">
+            {uniqueCategories.map((category) => (
+              <button
+                key={category as string}
+                onClick={() => setSelectedCategory(category as string)}
+                className={`px-4 py-2 rounded-full ${
+                  selectedCategory === (category as string)
+                    ? "bg-blue-600 text-white"
+                    : "bg-gray-200 text-gray-800"
+                }`}
+              >
+                {category as string}
+              </button>
+            ))}
+          </div>
         </header>
+
+        {/* Mostrar posts filtrados */}
+
         <div className="mx-auto mt-10 max-w-2xl border-t border-gray-200 pt-10 px-8 sm:mt-16 sm:pt-16 lg:mx-auto lg:max-w-full">
-          {posts.length > 0 ? (
+          {filteredPosts.length > 0 ? (
             <div className="grid grid-cols-1 gap-y-16 gap-x-8 lg:grid-cols-3">
-              {posts.map((post: BlogPost) => (
+              {filteredPosts.map((post: BlogPost) => (
                 <div key={post.id}>
                   <BlogCard key={post.id} post={post} />
                 </div>
@@ -47,7 +87,7 @@ export default function blog({
             </div>
           ) : (
             <div className="flex justify-center">
-              <h2 className="text-lg">No hay articulos disponibles.</h2>
+              <h2 className="text-lg">No hay artículos disponibles.</h2>
             </div>
           )}
         </div>
